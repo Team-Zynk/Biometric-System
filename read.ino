eac5d2d4c39cb6e2e1ab0513ce1d88a07aa319d3
Aman <guptaaaman14@gmail.com>
2023-12-23 16:24:36 +0530

    Optimized Read Function

--- read.ino
+++ read.ino
@@ -1,22 +1,16 @@
 int read1() {
     struct tm timeinfo;
     getLocalTime(&timeinfo);
-    String fileName = "/"+String(1900+timeinfo.tm_year) + String(1+timeinfo.tm_mon) + ".txt";
+    String fileName="/"+(String)roll+"/"+"Days Present"+".txt";
     tft.fillScreen(TFT_WHITE);
     tft.setCursor(40,10);
     tft.setTextSize(2);
     tft.setTextColor(TFT_BLUE);
     tft.println("Days Attended: ");
-    int d = 0;
     File dataFile;
     dataFile = SD.open(fileName,FILE_READ);
-    while (dataFile.available()) {
-      String line = dataFile.readStringUntil('\n');
-        int lineId = line.substring(0, line.indexOf(",")).toInt();
-        if (lineId == roll) {
-            d=d+1;
-          }
-    }
+    String line = dataFile.readStringUntil('\n');
+    int d= line.toInt();
     tft.setCursor(240,10);
     tft.print(d);
     tft.setCursor(40,100);
@@ -25,4 +19,5 @@ int read1() {
     delay(2500);
     dataFile.close();
     return d;
+
 }
