9fce3469f3b0f834438c3619f930bd5066bf9d87
Aman <guptaaaman14@gmail.com>
2023-12-23 16:16:17 +0530

    Updated Duplicate Attendance Case

--- write.ino
+++ write.ino
@@ -1,20 +1,41 @@
 void write1(){
 
-  pending();
+ 
   File dataFile;
   struct tm timeinfo;
   getLocalTime(&timeinfo);
   SD.mkdir("/"+(String)roll);
+ 
   String fileName="/"+(String)roll+"/"+"Dates Present"+".txt"; //Directory of Roll No. with file name as Dates Present
-
-
   String present_date = String(timeinfo.tm_mday) + "/"+String(1900+timeinfo.tm_year)+"/" + String(1+timeinfo.tm_mon) + ".txt";
+  String last_date="/"+(String)roll+"/"+"Last Date"+".txt";
+  dataFile = SD.open(last_date,FILE_READ);
+  String line = dataFile.readStringUntil('\n');
+  datafile.close();
+  if(line==present_date)
+  {tft.setCursor(40,160);
+  tft.setTextColor(TFT_BLUE);
+  tft.setTextSize(3);
+  tft.println("Already Marked");
+  delay(1000);
+  tft.fillScreen(TFT_WHITE);
+  read1();
+  return;
+  }
+  else
+  { SD.remove(last_date);
+  dataFile=SD.open(last_date,FILE_APPEND);
+  dataFile.println(present_date);
+  dataFile.close();
+  }
+  
+  pending();
   dataFile=SD.open(fileName,FILE_APPEND);
   dataFile.println(present_date);
   dataFile.close();
   fileName="/"+(String)roll+"/"+"Days Present"+".txt"; //Directory of Roll No. with file name as Days Present
   dataFile = SD.open(fileName,FILE_READ);
-  String line = dataFile.readStringUntil('\n');
+  line = dataFile.readStringUntil('\n');
   int present_days= line.toInt();
   present_days+=1;
   dataFile.close();
